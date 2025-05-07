99d83af754bd68c269d26ed65d033dc026ea014a
AndresT16 <146651291+AndresT16@users.noreply.github.com>
2025-05-07 09:44:16 -0500

    Añadir comillas a las variables

--- actualizador.sh
+++ actualizador.sh
@@ -3,10 +3,10 @@ set -e             #si algo falla el script se detiene
 
 os="/etc/os-release"
 
-if grep -qi "arch" $os;then                      # -qi supreme la salida de grep y le da igual si es mayuscula o minuscula
+if grep -qi "arch" "$os";then                      # -qi supreme la salida de grep y le da igual si es mayuscula o minuscula
     echo "distribucion basada en Arch"
     sudo pacman -Syu
-elif grep -qi "debian" $os || grep -qi "ubuntu" $os;then
+elif grep -qi "debian" "$os" || grep -qi "ubuntu" "$os";then
     echo "distribucion basada en Debian o ubuntu"
     sudo apt update && sudo apt upgrade                    # puedes poner -y para que instale sin preguntar
 else
